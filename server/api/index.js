c945d1ae01d20bab1dfa22b83d74bdd4ba7ec4ed
Zahid Hasan <mrzahidxy.com>
2025-01-10 21:59:53 +0600

    modify: app structure for vercel deploy

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 // Import routes
-const shortnerRouter = require("./route/shortner");
+const shortnerRouter = require("../route/shortner");
 
 // Initialize the app
 const app = express();
@@ -14,7 +14,15 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use("/", shortnerRouter);
+app.use("/api", shortnerRouter);
+
+// Health Check Endpoint
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "UP",
+    timestamp: new Date(),
+  });
+});
 
 // Start the server
 const PORT = process.env.PORT || 3000;
